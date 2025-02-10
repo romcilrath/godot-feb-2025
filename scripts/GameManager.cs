059b6da09bf191d126cc3fe615aee30615b5878d
Robert McIlrath <romcilrath@gmail.com>
2025-02-09 20:09:56 -0500

    Added GameManager.cs

---
+++ scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class GameManager : Node2D
+{
+    private static GameManager _instance;
+    public static GameManager Instance => _instance;
+
+    public override void _Ready() 
+    {
+        // Enforce singleton design pattern
+        if (_instance != null)
+        {
+            GD.PrintErr("Multiple GameManager instances detected! Deleting duplicate.");
+            QueueFree();
+            return;
+        }
+
+        _instance = this;
+        GD.Print("GameManager Initialized");
+    }
+}
